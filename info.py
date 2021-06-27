32f9ea8b6cdde0b64b99bf3c2a8373666ede2d07
Rediawan <79954806+Rediawan@users.noreply.github.com>
2021-06-27 14:51:53 +0800

    Add files via upload

---
+++ info.py
@@ -0,0 +1,35 @@
+import os, sys
+
+print ("\033[1;33mMasukan UserName&Password:)")
+print ("\033[1;31mKalo Gk Tau Liat YT gw Anjeng")
+username = 'Newbie'      
+password = 'Deface'
+
+def restart():
+	ngulang = sys.executable
+	os.execl(ngulang, ngulang, *sys.argv)
+
+def main():
+	uname = raw_input("username : ")
+	if uname == username:
+		pwd = raw_input("password : ")
+
+		if pwd == password:
+			print "\n\033[1;34mSelamat Datang di Script Yadi-Tools", 
+			sys.exit()
+
+		else:
+			print "\n\033[1;36mSorry Invalid Password !!!\033[00m"
+			print "Back Login\n"
+			restart()
+
+	else:
+		print "\n\033[1;36mSorry Invalid Username !!!\033[00m"
+		print "Back Login\n"
+		restart()
+
+try:
+	main()
+except KeyboardInterrupt:
+	os.system('clear')
+	restart()
